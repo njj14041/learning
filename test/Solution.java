769767f5fdbff9e1b5e4189c3802fcef878da1ab
dongguang <niujunjie.njj@alibaba-inc.com>
2026-03-09 16:09:13 +0800

    .

---
+++ test/Solution.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+
+    public int missingNumber(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {6, 9, 1, 4, 8, 7, 5, 3};
+        new Solution().f(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
