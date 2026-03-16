f4e8ae814b37eedbd5cf203fb36f4704148a0659
dongguang <niujunjie.njj@alibaba-inc.com>
2026-03-16 19:52:59 +0800

    .

--- test/Solution.java
+++ test/Solution.java
@@ -1,39 +1,35 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Solution {
 
-
-    public List<List<String>> f(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-        for (String str : strs) {
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String s = new String(chars);
-            List<String> list = map.get(s);
-            if (list == null) {
-                list = new ArrayList<>();
-            }
-            list.add(str);
-            map.put(s, list);
-        }
-        return new ArrayList<>(map.values());
+    public int fkl(int[] nums, int k) {
+        qs(nums, 0, nums.length - 1);
     }
 
+    private void qs(int[] nums, int left, int right) {
+        if (right >= left) return;
+        int p = partition(nums, left, right);
+        qs(nums, left, p);
+        qs(nums, p + 1, right);
+    }
 
+    private int partition(int[] nums, int left, int right) {
+        int temp = nums[left];
+        int i = left, j = right;
+        while (i<j) {
+            while (i < j && nums[left] <= temp) i++;
+            while (i < j && nums[right] > temp) j--;
 
-
-    // groupAnagrams
-    public static void main(String[] args) {
-        ListNode h = new Solution().removeNthFromEnd(node1, 2);
-        while (h != null) {
-            System.out.println(h.val);
-            h = h.next;
         }
-//        System.out.println(h);
+
+    }
+
+    // 1432219
+    //dailyTemperatures
+    public static void main(String[] args) {
+        System.out.println(new Solution().removeKdigits("1432219", 3));
+
     }
 
 }
