c8af395f9b3db2e18434ea286ebdd57590414d8b
dongguang <niujunjie.njj@alibaba-inc.com>
2026-03-18 16:14:43 +0800

    .

--- test/Solution.java
+++ test/Solution.java
@@ -1,9 +1,3 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 class Solution {
 
 
@@ -12,8 +6,8 @@ class Solution {
     public static void main(String[] args) {
         TreeNode treeNode2 = new TreeNode(2);
         TreeNode treeNode1 = new TreeNode(1, treeNode2, null);
-        List<List<Integer>> lists = new Solution().pathSum(treeNode1, 1);
-        lists.forEach(System.out::println);
+//        List<List<Integer>> lists = new Solution().pathSum(treeNode1, 1);
+//        lists.forEach(System.out::println);
     }
 
 }
