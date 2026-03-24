7974031f1f7a541678f64986410d2f33d724f876
dongguang <niujunjie.njj@alibaba-inc.com>
2026-03-24 16:57:31 +0800

    .

--- test/Solution.java
+++ test/Solution.java
@@ -1,21 +1,28 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
 
-    public int rob(TreeNode root) {
-        if (root == null) return 0;
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
 
-
-        return Math.max()
-
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
+        }
+        return Math.max(dp[n - 1][0], 0);
     }
-
 
     //findDuplicateSubtrees
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.rob(new int[]{2, 3, 2}));
+        ArrayList<String> wordDict = new ArrayList<>();
+        wordDict.add("aaaa");
+        wordDict.add("aaa");
+        System.out.println(solution.wordBreak("aaaaaaa", wordDict));
     }
 
 }
